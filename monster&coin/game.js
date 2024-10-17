28e370e58fd373ae1933b9b0bc2a1782c2741890
uabdirasul <ubbiniyazov04@gmail.com>
2024-10-17 18:12:07 +0500

    add asset

--- monster&coin/game.js
+++ monster&coin/game.js
@@ -1,13 +1,32 @@
 class mainScene {
   // Methods for the game:
   preload() {
-    // This method is called in the beginning and load all the assets
+    // Load all the assets
+    this.load.image("monster", "assets/monster.png");
+    this.load.image("coin", "assets/coin.png");
   }
+
   create() {
-    // This method is called after preload, it will create our scene
+    // Create the monster and coin sprites
+    this.monster = this.physics.add.sprite(100, 100, "monster");
+    this.coin = this.physics.add.sprite(300, 300, "coin");
+
+    // Set the size of the monster and coin
+    this.monster.setDisplaySize(50, 50); // Width, Height in pixels
+    this.coin.setDisplaySize(30, 30); // Width, Height in pixels
+
+    // Initialize score
+    this.score = 0;
+
+    // Style for the score text
+    let style = { font: "20px Arial", fill: "#fff" };
+
+    // Display the score
+    this.scoreText = this.add.text(20, 20, "score: " + this.score, style);
   }
+
   update() {
-    // This method is called every 60s after create, it will handle all the logic in the game.
+    // Handle all the logic in the game
   }
 }
 
