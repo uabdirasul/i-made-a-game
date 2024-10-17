4baca974a7711d1aeef20f043aa32fa5dd8326c9
uabdirasul <ubbiniyazov04@gmail.com>
2024-10-17 18:35:09 +0500

    add movements, border, coin collecting features

--- monster&coin/game.js
+++ monster&coin/game.js
@@ -23,10 +23,53 @@ class mainScene {
 
     // Display the score
     this.scoreText = this.add.text(20, 20, "score: " + this.score, style);
+
+    // Moving the "monster" :)
+    this.arrow = this.input.keyboard.createCursorKeys();
+
+    // Set world bounds
+    this.physics.world.setBounds(0, 0, 700, 400); // Set bounds to match the game size
+
+    // Enable bounds for the monster
+    this.monster.setCollideWorldBounds(true); // Prevent the monster from going out of bounds
+  }
+
+  hit() {
+    // Change the position x and y of the coin randomly
+    this.coin.x = Phaser.Math.Between(100, 600);
+    this.coin.y = Phaser.Math.Between(100, 300);
+
+    // Increment the score by 10
+    this.score += 10;
+
+    // Display the updated score on the screen
+    this.scoreText.setText("score: " + this.score);
   }
 
   update() {
     // Handle all the logic in the game
+
+    // Handle horizontal movements
+    if (this.arrow.right.isDown) {
+      // If the right arrow is pressed, move to the right
+      this.monster.x += 3;
+    } else if (this.arrow.left.isDown) {
+      // If the left arrow is pressed, move to the left
+      this.monster.x -= 3;
+    }
+
+    // Do the same for vertical movements
+    if (this.arrow.down.isDown) {
+      this.monster.y += 3;
+    } else if (this.arrow.up.isDown) {
+      this.monster.y -= 3;
+    }
+
+    // If the monster is overlapping with the coin
+    if (this.physics.overlap(this.monster, this.coin)) {
+      // Call the new hit() method
+      this.hit();
+    }
   }
 }
 
